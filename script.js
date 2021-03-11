8ddb9ee4e73618a68393f4c7cc6e5e0b7c506ace
smoloff <smoloff@gmail.com>
2021-03-12 00:22:18 +0200

    cat is existing

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
 class Cat {
-    constructor(tiredness, hunger, lonliness, happiness){
+    constructor(tiredness, hunger, lonliness, happiness) {
         this.tiredness = tiredness;
         this.hunger = hunger;
         this.lonliness = lonliness;
-        this.happiness = happiness;        
-        }
-    }
-    
-    feed(){
-        this.tiredness *=0.5;  
-        this.hunger *= 0;
-        this.lonliness /= .25; 
-        this.happiness /= .25;
-    }
-    
-    play(){
-
-    }
-    sleep(){
-
-    }
-    nightMode(){
-
+        this.happiness = happiness;
     }
 
-    hourCounter{
-        setInterval(1000); 
-        for (let i = 0; i < 24; i++)
+
+
+    checkWellness() {
+        setInterval(() => {
+            if (this.tiredness >= 90 && this.lonliness >= 90) {
+                this.sleep();
+            } else if (this.hunger >= 100 && this.lonliness >= 90) {
+                this.feed();
+            } else if (this.lonliness >= 90 && this.happiness <= 0) {
+                this.play();
+            } else if (this.happiness <= 0 && this.tiredness >= 90) {
+                this.sleep();
+            }
+            else {
+                this.chil();
+            }
+        }, 200)
     }
 
-    let newDay = new Cat(0, 100, 100, 50)
-    
+    sleep() {
+        this.tiredness -= 0.25;
+        this.hunger += 0.25;
+        this.happiness += 0.05;
+        console.log(`SLEEPING: Tiredness: ${this.tiredness} Hunger ${this.hunger} Lonliness ${this.lonliness} Happiness ${this.happiness}`);
+    }
+
+    feed() {
+
+        this.tiredness -= 0.005;
+        this.hunger -= 1.25;
+        this.lonliness -= 1.05;
+        this.happiness += 1.05;
+        console.log(`FEEDING: Tiredness: ${this.tiredness} Hunger ${this.hunger} Lonliness ${this.lonliness} Happiness ${this.happiness}`);
+
+
+    }
+
+    play() {
+        this.tiredness -= 0.25;
+        this.hunger += 0.25;
+        this.lonliness -= 0.05;
+        this.happiness += 0.25;
+        console.log(`PLAYING: Tiredness: ${this.tiredness} Hunger ${this.hunger} Lonliness ${this.lonliness} Happiness ${this.happiness}`);
+
+    }
+
+    chil() {
+
+        this.tiredness -= 0.05;
+        this.hunger += 0.005;        
+        this.happiness += 0.15;
+
+        console.log(`CHILING: Tiredness: ${this.tiredness} Hunger ${this.hunger} Lonliness ${this.lonliness} Happiness ${this.happiness}`);
+    }
 }
+
+
+let newDay = new Cat(100, 50, 100, 0);
+newDay.checkWellness();
+
